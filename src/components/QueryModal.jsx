490aa0a49d27a8f3278d8aecffe3a672d61861e9
Siddhant Kashyap <86836609+Siddhant-Kashyap@users.noreply.github.com>
2024-05-22 11:00:04 +0530

    QueryModal

--- src/components/QueryModal.jsx
+++ src/components/QueryModal.jsx
@@ -1,20 +1,43 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import axios from 'axios';
 
-const QueryModal = ({ isOpen, onClose,formData }) => {
+const QueryModal = ({ isOpen, onClose, formData }) => {
   const [isSpecialRequest, setIsSpecialRequest] = useState(false);
   const [specialRequestText, setSpecialRequestText] = useState("");
+  const [additionalData, setAdditionalData] = useState({
+    fullname: "",
+    email: "",
+    phone: "",
+    days: "",
+    budget: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAdditionalData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleOptionChange = (e) => {
     setIsSpecialRequest(e.target.value === "Yes");
   };
 
+
   const handleTextChange = (e) => {
     setSpecialRequestText(e.target.value);
   };
-  const handleSubmit=()=>{
-    console.log(formData);
-    onClose
-  }
+  const handleSubmit = async () => {
+    try {
+        const fullData = { ...formData, ...additionalData };
+        const response = await axios.post('https://travelopia-backend-4lw5.onrender.com/api/trips', fullData);
+        console.log(response.data); 
+        onClose();
+      } catch (error) {
+        console.error('Error:', error); // Log any errors
+      }
+  };
 
   return (
     isOpen && (
@@ -35,28 +58,43 @@ const QueryModal = ({ isOpen, onClose,formData }) => {
           <div className="mb-4">
             <input
               type="text"
+              name="fullname"
+              value={additionalData.fullname}
+              onChange={handleChange}
               placeholder="Full Name *"
               className="w-full p-2 border rounded-md mb-2 text-black "
             />
             <input
               type="text"
+              name="email"
+              value={additionalData.email}
+              onChange={handleChange}
               placeholder="Email *"
               className="w-full p-2 border rounded-md mb-2 text-black "
             />
             <input
               type="tel"
+              name="phone"
+              value={additionalData.phone}
+              onChange={handleChange}
               placeholder="Enter phone number"
               className="w-full p-2 border rounded-md mb-2 text-black "
             />
             <div className="flex">
               <input
                 type="number"
+                name="days"
+                value={additionalData.days}
+                onChange={handleChange}
                 placeholder="Number of days"
                 className="w-full p-2 border rounded-md mb-2 text-black "
               />
               <input
                 type="number"
                 placeholder="Budget Per Person"
+                name="budget"
+                value={additionalData.budget}
+                onChange={handleChange}
                 className="w-full p-2 border rounded-md mb-2 text-black "
               />
             </div>
@@ -103,14 +141,17 @@ const QueryModal = ({ isOpen, onClose,formData }) => {
                     type="text"
                     value={specialRequestText}
                     onChange={handleTextChange}
-                    style={{ border: '1px solid black' }}
+                    style={{ border: "1px solid black" }}
                     className="text-black w-full"
                   />
                 </label>
               </div>
             )}
           </div>
-          <button className="bg-[#F26A47] text-white px-4 py-2 rounded-md w-full" onClick={handleSubmit}>
+          <button
+            className="bg-[#F26A47] text-white px-4 py-2 rounded-md w-full"
+            onClick={handleSubmit}
+          >
             GET MY QUOTE
           </button>
         </div>
